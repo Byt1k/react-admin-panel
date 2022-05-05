edc7c1c0b6523274f8fd558ae3a8b08e279add64
Byt1k <byt1kip82@gmail.com>
2022-05-05 12:24:53 +0300

    added datatable for users

---
+++ src/components/datatable/Datatable.tsx
@@ -0,0 +1,31 @@
+import './datatable.scss'
+import {DataGrid, GridColDef, GridValueGetterParams} from '@mui/x-data-grid';
+import {userColumns, userRows} from "../../datatablesource";
+
+export const Datatable = () => {
+    const actionColumn: GridColDef[] = [{
+        field: 'action',
+        headerName: 'Action',
+        width: 200,
+        renderCell: params => {
+            return (
+                <div className='cellAction'>
+                    <div className="viewButton">View</div>
+                    <div className="deleteButton">Delete</div>
+                </div>
+            )
+        }
+    }]
+
+    return (
+        <div className='datatable'>
+            <DataGrid
+                rows={userRows}
+                columns={userColumns.concat(actionColumn)}
+                pageSize={10}
+                rowsPerPageOptions={[10]}
+                checkboxSelection
+            />
+        </div>
+    )
+}
